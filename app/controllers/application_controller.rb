b89ac8ce41acfd4c7b0cec8a82469ca669483e34
本津　慧人 <137137801+keitootiek@users.noreply.github.com>
2023-08-17 15:38:29 +0900

    商品一覧機能実装#出品した商品が左から日付順に表示される

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :kanji_last_name, :kanji_first_name, :kana_last_name, :kana_first_name, :birthday_yyyy_mm_dd])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :kanji_last_name, :kanji_first_name, :kana_last_name, :kana_first_name,
+                                             :birthday_yyyy_mm_dd])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_16_061203) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
-
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", null: false
     t.string "encrypted_password", null: false
