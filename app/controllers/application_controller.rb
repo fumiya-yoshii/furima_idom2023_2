af13153142f556ab89168581188e413cdfcab4ce
fumiya-yoshii <135097414+fumiya-yoshii@users.noreply.github.com>
2023-08-17 15:21:47 +0900

    環境変数を読み込む記述に変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   # ベーシック認証
     def basic_auth
       authenticate_or_request_with_http_basic do |username, password|
-        username == 'idom' && password == '2023'
-    end
+        username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+      end
   end
 
   def configure_permitted_parameters
