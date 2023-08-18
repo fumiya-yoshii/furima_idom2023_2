8e5f8c5788befe32f676ad52d2cd522ab1150e02
fumiya-yoshii <135097414+fumiya-yoshii@users.noreply.github.com>
2023-08-18 15:36:48 +0900

    OrdersController 作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,19 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    @order_address.save
+    redirect_to root_path
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_num, :building_name,:phone_num).merge(item_id: @item.id , user_id: current_user.id)
   end
 end
