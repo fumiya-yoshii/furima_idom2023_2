018eece233a49d8502737dee84756a967c9be695
fumiya-yoshii <135097414+fumiya-yoshii@users.noreply.github.com>
2023-08-18 15:37:46 +0900

    オーダールーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :items, only: [:index, :new, :create, :show, :destroy] do
-    resources :orders, only: [:index]
+    resources :orders, only: [:index, :new, :create]
   end
 end
