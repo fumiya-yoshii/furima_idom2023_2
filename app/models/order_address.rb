7184b6a5c38cbf8a471324517f72de79f8af9af8
fumiya-yoshii <135097414+fumiya-yoshii@users.noreply.github.com>
2023-08-21 11:44:10 +0900

    オーダーテスト

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,14 +3,14 @@ class OrderAddress
   attr_accessor :postal_code, :prefecture_id, :city, :house_num, :building_name, :phone_num, :item_id, :user_id, :price, :token
 
   with_options presence: true do
+    validates :token
     validates :user_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :city
     validates :house_num
-    validates :phone_num
+    validates :phone_num, length: { minimum: 10 ,maximum: 11 }, format: { with: /\A[0-9]+\z/ }
     validates :price
-    validates :token
   end
  
   def save
